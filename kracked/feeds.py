5de23fff5814ce5a3cc2168af72f8031af45e882
r2stanton <stantor@clarkson.edu>
2024-09-25 23:03:26 -0600

    fully functional L3

--- kracked/feeds.py
+++ kracked/feeds.py
@@ -18,38 +18,57 @@ class KrakenL3(BaseKrakenWS):
         self.trace = trace
         self.api_key = api_key
         self.api_secret = secret_key
+        self.write_every = write_every
+        self.out_file_name = out_file_name
+        self.ticks = []
 
     def _on_message(self, ws, message):
         response = json.loads(message)
-        if 'data' in response.keys():
+        if len(self.ticks) > self.write_every:
+            with open(self.out_file_name, "a+") as fil:
+                for tick in self.ticks:
+                    tick = [str(t) for t in tick]
+                    fil.write(",".join(tick)+"\n")
+            self.ticks = []
+            self.tick_count = 0
+
+        # print(response)
+        if 'data' in response.keys() and response['type'] != 'snapshot':
             assert len(response['data']) == 1 , "Haven't seen this response before"
-            print(response)
-            bids = response['data'][0]['bids']
-            asks = response['data'][0]['asks']
+            if 'bids' in response['data'][0].keys() and 'asks' in response['data'][0].keys():
+                bids = response['data'][0]['bids']
+                asks = response['data'][0]['asks']
+                if len(bids) > 0:
+                    for bid in bids:
+                        print(bid)
+                        info = ['b',                # Side
+                                bid['timestamp'],   # Time
+                                bid['limit_price'], # Price
+                                bid['order_qty'],   # Size
+                                bid['event'],       # Event
+                                bid['order_id'],    # OID
+                                ]
+                        self.ticks.append(info)
+                        self.tick_count += 1
+                if len(asks) > 0:
+                    for ask in asks:
+                        info = ['a',                # Side
+                                ask['timestamp'],   # Time
+                                ask['limit_price'], # Price
+                                ask['order_qty'],   # Size
+                                ask['event'],       # Event
+                                ask['order_id'],    # OID
+                                ]
+                        self.ticks.append(info)
+                        self.tick_count += 1
+                if len(asks) == 0 and len(bids) == 0:
+                    print(response)
 
-            if len(bids) > 0:
-                for bid in bids:
-                    info = ['b',                # Side
-                            bid['timestamp'],   # Time
-                            bid['limit_price'], # Price
-                            bid['order_qty'],   # Size
-                            bid['event'],       # Event
-                            bid['order_id'],    # OID
-                            ]
-                    print(info)
-            if len(asks) > 0:
-                for ask in asks:
-                    info = ['a',                # Side
-                            bid['timestamp'],   # Time
-                            bid['limit_price'], # Price
-                            bid['order_qty'],   # Size
-                            bid['event'],       # Event
-                            bid['order_id'],    # OID
-                            ]
-                    print(info)
 
-
-        self.tick_count += 1
+        elif 'data' in response.keys() and response['type'] == 'snapshot':
+            # Decide what to do with initial snapshot later
+            print("SKIPPING SNAPSHOT")
+            pass
 
 
     def _on_open(self, ws):
