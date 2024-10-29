eb7d7670ac8295ad7a2ecd97d0048a0642a68410
r2stanton <stantor@clarkson.edu>
2024-10-28 18:07:25 -0600

    l3 example file finished

--- examples/L3_example.py
+++ examples/L3_example.py
@@ -1,24 +1,13 @@
-from kracked.feeds import KrakenL3, KrakenL2
-import toml, os
+from kracked.feeds import KrakenL3
+import toml
 
-
-if not os.path.exists("data"):
-    os.system("mkdir data")
-
-
-# Load in your API and SECRET key with the preferred method.
-# Here is an example to load from a toml file.
+# Load/type directly as arguments to the KrakenL3 constructor your API info.
 with open(f"/home/alg/.api.toml", "r") as fil:
     data = toml.load(fil)
 api_key = data['kraken_api']
 api_secret = data['kraken_sec']
 
-if not os.path.exists("data/"):
-    os.system("mkdir data")
 l3feed = KrakenL3(["DOGE/USD", "BTC/USD", "ETH/USD", "SOL/USD"],
-                  api_key=api_key,
-                  secret_key=api_secret,
-                  trace=False,
-                  out_file_name='data/output.csv')
-
+                  api_key=api_key, secret_key=api_secret,
+                  trace=False, out_file_name='./output.csv')
 l3feed.launch()
