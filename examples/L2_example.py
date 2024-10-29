1023c9324ce981ca6f550d2b8e4b51b991896022
r2stanton <stantor@clarkson.edu>
2024-10-28 18:07:17 -0600

    L2 example file finished

--- examples/L2_example.py
+++ examples/L2_example.py
@@ -1,30 +1,4 @@
-from kracked.feeds import KrakenL3, KrakenL2
-import toml, os
-
-
-if not os.path.exists("data"):
-    os.system("mkdir data")
-
-
-# Load in your API and SECRET key with the preferred method.
-# Here is an example to load from a toml file.
-with open(f"/home/alg/.api.toml", "r") as fil:
-    data = toml.load(fil)
-api_key = data['kraken_api']
-api_secret = data['kraken_sec']
-
-if not os.path.exists("data/"):
-    os.system("mkdir data")
-
-
-l2feed = KrakenL2("BTC/USD",
-                  api_key=api_key,
-                  secret_key=api_secret,
-                  trace=False,
-                  depth=10,
-                  output_directory="data",
-                  log_book_every=100,
-                  log_bbo_every=10,
-                  )
-
+from kracked.feeds import KrakenL2
+l2feed = KrakenL2("BTC/USD", trace=False, depth=10, output_directory=".",
+                  log_book_every=100, log_bbo_every=10)
 l2feed.launch()
