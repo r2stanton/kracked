5d7ebce03973257797ef45889bd3aa3913d81794
r2stanton <stantor@clarkson.edu>
2024-10-30 23:27:30 -0600

    example changes

--- examples/L1_example.py
+++ examples/L1_example.py
@@ -1,7 +1,7 @@
 from kracked.feeds import KrakenL1
 
 l1feed = KrakenL1(
-    ["DOGE/USD", "BTC/USD"],    # List or str of requested symbols.
+    "DOGE/USD",    # List or str of requested symbols.
     output_directory="ex_out",  # Output directory for L1 data files.
     trace=False,                # Do not clog output with WS info. 
 )
--- examples/OHLC_example.py
+++ examples/OHLC_example.py
@@ -1,8 +1,9 @@
 from kracked.feeds import KrakenOHLC
 
 ohlcfeed = KrakenOHLC(
-    ["BTC/USD", "DOGE/USD"],# List or str of requested symbols.
-    interval=1,             # Interval in minutes.
+    # ["BTC/USD", "DOGE/USD"],# List or str of requested symbols.
+    ["DOGE/USD"],# List or str of requested symbols.
+    interval=5,             # Interval in minutes.
     output_directory="ex_out",# Output directory for L1 data files.
     trace=False,            # Do not clog output with WS info.
 )
