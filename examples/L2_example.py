3f782f1afb68a494bcc3d7c7e2eb455e94ed55f0
r2stanton <stantor@clarkson.edu>
2024-11-09 23:47:50 -0700

    example for l2 book fixed

--- examples/L2_example.py
+++ examples/L2_example.py
@@ -1,8 +1,9 @@
 from kracked.feeds import KrakenL2
 
 l2feed = KrakenL2(
-    ["BTC/USD", "DOGE/USD"],    # List or str of requested symbols.
-    log_book_every=100,         # How often to log L2 book info.
+    ["BTC/USD"],    # List or str of requested symbols.
+    # ["DOGE/USD"],    # List or str of requested symbols.
+    log_book_every=1,         # How often to log L2 book info.
     append_book=True,           # Append book mode, saves historical L2 data.
     output_directory="ex_out",  # Output directory for L2 data files.
     trace=False,                # Do not clog output with WS info.
