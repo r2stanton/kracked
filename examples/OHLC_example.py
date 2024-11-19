6e02f64c3cfc5c0f597d654c64e8ca0a17d5e895
r2stanton <stantor@clarkson.edu>
2024-11-18 18:44:54 -0700

    add in longer rest api candlestick history

--- examples/OHLC_example.py
+++ examples/OHLC_example.py
@@ -6,6 +6,13 @@ ohlcfeed = KrakenOHLC(
     interval=5,             # Interval in minutes.
     output_directory="ex_out",# Output directory for L1 data files.
     trace=False,            # Do not clog output with WS info.
+    ccxt_snapshot=True,     # Use the ccxt snapshot (you get more data)
 )
+
+# BEWARE if you use ccxt_snaphot, you get 720 historical candles, HOWEVER
+# they will not contain information about VWAP, TRADES, etc.
+
+# If you choose not to use ccxt for the historical data from Kraken, you will
+# only get 5 historical candlesticks.
 
 ohlcfeed.launch()
